2cf24a474b93480fc7bb841cc6ce76219fe0afdd
Peter Leng <peterleng1234@gmail.com>
2021-04-18 21:35:55 +0100

    Code cleanup

--- src/lib/pca9685.cpp
+++ src/lib/pca9685.cpp
@@ -9,8 +9,7 @@
 PCA9685::PCA9685(int i2c_bus, int device_address) :
         i2c_bus_(i2c_bus), device_address_(device_address) {}
 
-PCA9685::PCA9685(int device_address) :
-        i2c_bus_(1), device_address_(device_address) {}
+PCA9685::PCA9685(int device_address): PCA9685(1, device_address) {}
 
 int PCA9685::connect() {
     file_descriptor_ = wiringPiI2CSetup(device_address_);
